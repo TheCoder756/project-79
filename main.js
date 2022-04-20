779c18817bc583ef14f5aa4c115207f8e9b03092
TheCoder756 <92489997+TheCoder756@users.noreply.github.com>
2022-04-20 18:59:46 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -8,19 +8,17 @@ menu_list_array = ["Veg Margherita Pizza",
     function getmenu(){
         var htmldata="";
         menu_list_array.sort();
-        for(var i=0;i<menu_list_array.length;i++);
-        {
+        for(var i=0;i<menu_list_array.length;i++){
             htmldata=htmldata+ menu_list_array[i] + '<br>'
         }
-        document.getElementById("display_menu").innerHTML = htmldata;
+        document.getElementById("").innerHTML = htmldata;
         //give the appropriate id name as display_menu 
     }
 
     function add_item(){
         var htmldata;
 		var imgtags='<img id="im1" src="pizzaImg.png">'
-        var item=document.getElementById("add_item");.value;
-        menu_list_array.sort();
+        var item=document.getElementById("add_item").value;
         // use the sort function as - menu_list_array.sort();
         htmldata="";
         for(var i=0;i<menu_list_array.length;i++){
